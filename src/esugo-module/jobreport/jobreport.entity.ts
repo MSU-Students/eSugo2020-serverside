6c2d6bed4c926a064e207e85498cc6bd43887cf3
ruzlicali16 <ruzlicali24@gmail.com>
2021-07-21 13:18:48 +0800

    job report entity

---
+++ src/esugo-module/jobreport/jobreport.entity.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { CreateJobreportDto } from "src/dto/create-jobreport.dto";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Jobreport implements CreateJobreportDto {
+
+    @ApiProperty()
+    @PrimaryGeneratedColumn() 
+    jobreportID: number;
+        
+    @ApiProperty()
+    @Column({nullable: false})
+    userID: number;
+
+    @ApiProperty()
+    @Column({nullable: false}) 
+    jobID: number;
+
+}
---
+++ src/esugo-module/jobreport/jobreport.service.ts
@@ -0,0 +1,25 @@
+import { Jobreport } from './jobreport.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateJobreportDto } from 'src/dto/create-jobreport.dto';
+import { JobreportDto } from 'src/dto/jobreport.dto';
+import { Repository } from 'typeorm';
+@Injectable()
+export class JobreportService {
+    constructor(@InjectRepository(Jobreport) private jobreportRepository:Repository<Jobreport>) {}
+    async create(jobreport: CreateJobreportDto): Promise<JobreportDto> {
+        return this.jobreportRepository.save(jobreport);
+    }
+    findOne(id: number) {
+        return this.jobreportRepository.findOne(id);
+    }
+    findAll(): Promise<JobreportDto[]> {
+        return this.jobreportRepository.find();
+    }
+    async update(id: number, jobreport: JobreportDto) {
+        await this.jobreportRepository.update(id, jobreport)
+    }
+   async delete(id: number) {
+       await this.jobreportRepository.delete(id);
+    }
+}
